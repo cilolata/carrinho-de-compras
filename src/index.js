8a2215458d023e74e9434c162439c4071f1287f8
cilolata <cilolata@gmail.com>
2020-05-10 18:35:21 -0300

    refatorando o código para funções separadas

--- src/index.js
+++ src/index.js
@@ -1,76 +1,75 @@
+const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
 
- const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
-
-  function getShoppingCart(ids, productsList) {
-
-  //buscando os produdutos por id
-  const products = productsList.filter(product => ids.includes(product.id));
-
-  //retornando apenas name e category
+function getProductsCart(products){
   const productsItems = products.map(product => { 
     return { 
       name: product.name, 
       category: product.category
     }
   });
+    
+    return productsItems
+}
 
-  //buscando apenas category 
-  const category = products.map(product => product.category);
+function getPromotion(products, promotions) {
+  const categories = []
+  products.map(product => {
+    if(!categories.includes(product.category))
+      categories.push(product.category)
+     
+  })
 
-  //contando a qtda de cada categoria 
-  const categoryReduce = category.reduce( (object , item) =>{  
-    if ( !object[item] ) {
-      object[item] = 1;
-    } else {
-      object[item]++;
-    }
-      return object; 
-    },[]);
+    return promotions[categories.length - 1]
+}
 
-  //retornando apenas os item (valores) das categorias
-  const numberOfCategories = Object.values(categoryReduce);
-
-  //retornando o maior numero (entre as categorias)
-  const categoryPromotion = numberOfCategories.reduce((a,b) =>{ 
-	  return Math.max(a, b)
-  },[]);
-
-  //verificando qual é a promoção 
-  const promotion = categoryPromotion >= 4 ? 
-	promotions[0] : categoryPromotion === 1 ? 
-	promotions[3] : promotions[categoryPromotion - 1];
-
-  //somando os preços sem desconto
-  const regularTotalPrice = products.reduce((initialValue, product) => {
-    return initialValue + product.regularPrice
-  }, 0.0);
-
-  //verificando se a promoção se aplica e retornando o valor total com promoção
+function getTotalPrice(products, promotion) {
   const totalPrice = products.reduce((initialPrice, product) => {
     let productPromotion = product.promotions.find((productPromotion) => {
       return productPromotion.looks.includes(promotion)
-    });
-    if (!productPromotion) {
-      productPromotion = { price: product.regularPrice };
-    }
-      return initialPrice + productPromotion.price;
-    }, 0.0).toFixed(2)
+    })
 
+      if (!productPromotion) {
+        productPromotion = { price: product.regularPrice };
+      }
 
+        return initialPrice + productPromotion.price;
+  }, 0.0).toFixed(2)
+    
+        return totalPrice
+}
+  
+function getRegularPrice(products) {
+  const regularTotalPrice = products.reduce((initialValue, product) => {
+    return initialValue + product.regularPrice
+  }, 0.0);
+ 
+    return regularTotalPrice;
+}
 
+function getShoppingCart(ids, productsList) {
+  const products = productsList.filter(product => ids.includes(product.id));
+  const productsNameCategory = getProductsCart(products);
+  const promotion = getPromotion(products, promotions)
+  const totalPrice = getTotalPrice(products, promotion);
+  const regularTotalPrice = getRegularPrice(products);
   const discountValue = (regularTotalPrice - totalPrice).toFixed(2);
   const discount = ((discountValue / regularTotalPrice ) * 100).toFixed(2) + "%";
 
+ 
   return {
-    products: productsItems,
+    products: productsNameCategory,
     promotion,
     totalPrice,
     discountValue,
     discount
-  };
-
+  }
+    
 }
 
 module.exports = { getShoppingCart };
+    
+    
+
+
 
 
